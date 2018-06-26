116b8dceb290e117c0773b13878b0f283a469c4d
qiweixiang <qiweixiang@ZB-PF10V191.360buyAD.local>
2018-06-26 11:43:58 +0800

    c

--- src/com/jd/Shop.java
+++ src/com/jd/Shop.java
@@ -8,7 +8,9 @@ public class Shop {
 		System.out.println("dev01");
 		System.out.println("master02");
 		System.out.println("dev02");
-		System.out.println("master03");
-		System.out.println("master04");
+		System.out.println("master03   hahah     dev    03");
+		System.out.println("dev      ahhahh ");
+		
+		System.out.println("master04   hahah          dev 03");
 	}
 }
